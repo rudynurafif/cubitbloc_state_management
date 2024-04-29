77e58d725ce766fb1617f6cbe6b73887f3c530dd
Rudy Nurafif <rudynurafif@gmail.com>
2024-04-29 07:48:17 +0700

    bloc observer onChange

--- lib/basic_cubit.dart
+++ lib/basic_cubit.dart
@@ -5,6 +5,9 @@ import 'package:flutter/widgets.dart';
 class CounterCubit extends Cubit<int> {
   int initialData;
 
+  int? current;
+  int? next;
+
   CounterCubit({this.initialData = 0}) : super(initialData);
 
   void tambahData() {
@@ -14,10 +17,28 @@ class CounterCubit extends Cubit<int> {
   void kurangData() {
     emit(state - 1);
   }
+
+  // Bloc fitur untuk memantau perubahan data atau state
+
+  // onChange
+  @override
+  void onChange(Change<int> change) {
+    super.onChange(change);
+    print(change);
+    current = change.currentState;
+    next = change.nextState;
+  }
+
+  // onError
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    // TODO: implement onError
+    super.onError(error, stackTrace);
+  }
 }
 
 class BasicCubit extends StatelessWidget {
-  CounterCubit myCounter = CounterCubit(initialData: 65);
+  CounterCubit myCounter = CounterCubit();
 
   @override
   Widget build(BuildContext context) {
@@ -37,11 +58,26 @@ class BasicCubit extends StatelessWidget {
             initialData: myCounter.initialData,
             stream: myCounter.stream,
             builder: (context, snapshot) {
-              return Text(
-                "${snapshot.data}",
-                style: TextStyle(fontSize: 50),
+              return Column(
+                children: [
+                  Text(
+                    "${snapshot.data}",
+                    style: TextStyle(fontSize: 50),
+                  ),
+                  Text(
+                    "Current : ${myCounter.current}",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  Text(
+                    "Next : ${myCounter.next}",
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ],
               );
             },
+          ),
+          SizedBox(
+            height: 30,
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
