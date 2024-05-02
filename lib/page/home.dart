d9a7a09490ab7f9b4f253644dd2cb1b38d75252d
Rudy Nurafif <rudynurafif@gmail.com>
2024-05-02 08:52:00 +0700

    Bloc - Extension method

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -7,7 +7,19 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    UserBloc userBloc = context.read<UserBloc>();
+    /*
+      Extension method
+      1. context.read() -> biasanya digunakan didalam function
+      2. context.watch() -> digunakan setiap ada perubahan state
+      3. context.select()
+    */
+
+    UserBloc userBloc = context.read<UserBloc>(); // Membaca sekali
+    // UserBloc userBloc = context.watch<UserBloc>(); // Membaca berulang kali setiap ada perubahan state pada bloc
+
+    // Membaca berulang kali setiap ada perubahan state pada bloc
+    String nameUser = context.select<UserBloc, String>((value) => value.state["name"]);
+    int ageUser = context.select<UserBloc, int>((value) => value.state["age"]);
 
     return Scaffold(
       appBar: AppBar(
@@ -17,34 +29,8 @@ class HomePage extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(20),
         children: [
-          // BlocBuilder<UserBloc, Map<String, dynamic>>(
-          //   bloc: userBloc,
-          //   builder: (context, state) {
-          //     print("Nama");
-          //     return Text("Nama: ${state['name']}");
-          //   },
-          // ),
-          // BlocBuilder<UserBloc, Map<String, dynamic>>(
-          //   bloc: userBloc,
-          //   builder: (context, state) {
-          //     print("Umur");
-          //     return Text("Umur: ${state["age"]} tahun");
-          //   },
-          // ),
-          BlocSelector<UserBloc, Map<String, dynamic>, String>(
-            selector: (state) => state['name'],
-            builder: (context, state) {
-              print("Nama");
-              return Text("Nama: $state");
-            },
-          ),
-          BlocSelector<UserBloc, Map<String, dynamic>, int>(
-            selector: (state) => state['age'],
-            builder: (context, state) {
-              print("Umur");
-              return Text("Umur: $state");
-            },
-          ),
+          Text("Nama : $nameUser"),
+          Text("Umur : $ageUser"),
           const SizedBox(
             height: 20,
           ),
